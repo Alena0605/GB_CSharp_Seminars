c849c6094c330d8127724c90521584db27222b2a
Alena <ABelova0605@gmail.com>
2022-07-16 01:36:01 +0300

    add Task001 in folder Seminar_1

---
+++ Seminar_1/Task001_Sqrt/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+result = number * number;
+Console.WriteLine($"{number} ^ 2 = {result}");
