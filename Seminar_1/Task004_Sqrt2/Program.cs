b6fef890a236d50daa437b3aea0f0ee6efa2638f
Alena <ABelova0605@gmail.com>
2022-07-16 02:47:03 +0300

    add Task004 in folder Seminar_1

---
+++ Seminar_1/Task004_Sqrt2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу вычисления значения функции возведения числа в квадрат.
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(number1));
+
+Console.Write("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+PowerProcedural(number2);
+
+
+// Функция
+int Power(int currentNumber)
+{
+    return currentNumber * currentNumber;
+}
+
+// Процедура
+void PowerProcedural(int currentNumber)
+{
+    Console.WriteLine(currentNumber * currentNumber);
+}
