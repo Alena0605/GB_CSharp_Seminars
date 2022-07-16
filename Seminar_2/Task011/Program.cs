839b61686027c55764c1a615a4a510b5cc9b767a
Alena <ABelova0605@gmail.com>
2022-07-16 23:25:03 +0300

    add folder Task011 in Seminar_2

---
+++ Seminar_2/Task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine(number);
+
+int digitHundreds = number / 100;
+int digitHundredsRemaind = number % 100;
+int digitTens = digitHundredsRemaind / 10;
+int digitOnes = digitHundredsRemaind % 10;
+
+Console.WriteLine(digitHundreds * 10 + digitOnes);
