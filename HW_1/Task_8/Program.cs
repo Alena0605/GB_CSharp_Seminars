9dba02d700fe39f382e72a1d237a73bb377d3b2d
Alena <ABelova0605@gmail.com>
2022-07-16 12:19:52 +0300

    add folder Task_8 in HW_1

---
+++ HW_1/Task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿// 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (number <= 0)
+{
+    Console.WriteLine("Нужно ввести целое положительное число.");
+}
+else
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write(count + " ");
+        }
+        count++;
+    }
+}
+
+Console.WriteLine();
