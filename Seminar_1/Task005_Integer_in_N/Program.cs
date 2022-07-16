ca0934065ef0a969639137a1f5c47b4b1d59a0fa
Alena <ABelova0605@gmail.com>
2022-07-16 03:12:15 +0300

    add Task005 in folder Seminar_1

---
+++ Seminar_1/Task005_Integer_in_N/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = -number;
+
+while (startNumber != (number + 1))
+{
+    Console.Write(startNumber + ",");
+    startNumber++;
+}
+
+Console.Write("\b.");
+Console.WriteLine();
