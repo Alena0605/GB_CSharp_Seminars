7b017f89e8bf54e938129607f67182daf14e7b05
Alena <ABelova0605@gmail.com>
2022-07-17 10:15:27 +0300

    add folder Task013 in HW_2

---
+++ HW_2/Task013/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Напишите программу, которая с помощью деления выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > -100 && number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999 || number < -999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine(number %= 10);
+}
