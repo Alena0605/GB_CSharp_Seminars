e00bb2ed41c63245596b3f22fe454c8baa8a389b
Alena <ABelova0605@gmail.com>
2022-07-21 20:48:00 +0300

    add folder Task019 in HW_3

---
+++ HW_3/Task019/Program.cs
@@ -0,0 +1,23 @@
+﻿// 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int copyNumber = number;
+int reverse = 0;
+int remainder;
+
+if (number > 9999 && number < 100000)
+{
+    while (copyNumber > 0)
+    {
+        remainder = copyNumber % 10;
+        reverse = (reverse * 10) + remainder;
+        copyNumber /= 10;
+    }
+
+    Console.WriteLine(reverse);
+
+    if (reverse == number) Console.WriteLine($"Число {number} является полиндромом.");
+    else Console.WriteLine($"Число {number} НЕ является полиндромом.");
+}
+else Console.WriteLine("Неверный ввод.");
