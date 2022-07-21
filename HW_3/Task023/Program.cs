2819b24ca7bd2866d738b292562814632d84791d
Alena <ABelova0605@gmail.com>
2022-07-21 21:06:15 +0300

    add folder Task023 in HW_3

---
+++ HW_3/Task023/Program.cs
@@ -0,0 +1,13 @@
+﻿// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Task023()
+{
+    Random random = new Random();
+    int number = random.Next(1, 10);
+    Console.WriteLine($"Задано число {number}");
+    
+    for (int i = 1; i <= number; i++) Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
+
+for (int i = 1; i < 5; i++)
+Task023();
