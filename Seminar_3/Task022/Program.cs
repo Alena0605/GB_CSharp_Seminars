a2d15d9e5325ea8cf842249fd87ce1d8a3cabdb0
Alena <ABelova0605@gmail.com>
2022-07-21 19:46:41 +0300

    add folder Task022 in Seminar_3

---
+++ Seminar_3/Task022/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void Task022()
+{
+    Random random = new Random();
+    int number = random.Next(1, 10);
+    Console.WriteLine($"Задано число {number}");
+    
+    for (int i = 1; i <= number; i++) Console.WriteLine($"{i}^2 = {i * i}");
+}
+
+for (int i = 1; i < 5; i++)
+Task022();
