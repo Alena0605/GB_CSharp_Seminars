c4008e993ad6fee3cda12d22b57674b120cc4400
Alena <ABelova0605@gmail.com>
2022-07-23 20:57:15 +0300

    add folder Task027 in HW_4

---
+++ HW_4/Task027/Program.cs
@@ -0,0 +1,17 @@
+﻿// 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int copyNumber = number;
+int remainder;
+int result = 0;
+
+while (copyNumber > 0)
+{
+    remainder = copyNumber % 10;
+    result += remainder;
+    copyNumber /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе {number} = {result}");
