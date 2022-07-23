9c3afef42448763dbb57001c3986c3fd39a6d339
Alena <ABelova0605@gmail.com>
2022-07-23 18:50:49 +0300

    add folder Task028 in Seminar_4

---
+++ Seminar_4/Task028/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+void Task28()
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 10);
+    Console.WriteLine($"Число {number}");
+
+    int mult = 1;
+    int i = 1;
+
+    while (i <= number)
+    {
+        mult *= i;
+        Console.Write(i + " * ");
+        i++;
+    }
+
+    Console.Write("\b\b= ");
+    Console.WriteLine(mult);
+}
+
+for (int i = 1; i < 5; i++)
+Task28();
