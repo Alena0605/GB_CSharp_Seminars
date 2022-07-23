5be474b85f23dc172df230481a96376ee6457235
Alena <ABelova0605@gmail.com>
2022-07-23 18:17:18 +0300

    add folder Task024 in Seminar_4

---
+++ Seminar_4/Task024/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+void Task24()
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 10);
+    Console.WriteLine($"Число {number}");
+
+    int sum = 0;
+    int i = 1;
+
+    while (i <= number)
+    {
+        sum += i;
+        Console.Write(i + " + ");
+        i++;
+    }
+
+    Console.Write("\b\b= ");
+    Console.WriteLine(sum);
+}
+
+for (int i = 1; i < 5; i++)
+Task24();
