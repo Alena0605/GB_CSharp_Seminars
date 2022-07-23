e348251afb07d10a770bf1abb8f50b7f64b764ff
Alena <ABelova0605@gmail.com>
2022-07-23 18:37:06 +0300

    add folder Task026 in Seminar_4

---
+++ Seminar_4/Task026/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (number > 0)
+{
+    number /= 10;
+    count++;
+}
+
+Console.WriteLine("Цифр в числе: " + count);
