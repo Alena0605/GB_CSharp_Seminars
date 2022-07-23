7633589592c184179a06cc22c7f92233c11b3589
Alena <ABelova0605@gmail.com>
2022-07-23 20:44:57 +0300

    add folder Task025 in HW_4

---
+++ HW_4/Task025/Program.cs
@@ -0,0 +1,22 @@
+﻿// 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Pow(int num1, int num2)
+{
+    int result = 1;
+
+    for (int i = 0; i < num2; i++)
+    {
+        result *= num1;
+    }
+
+    Console.WriteLine($"{num1}^{num2} = {result}");
+}
+
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Pow(numberA, numberB);
