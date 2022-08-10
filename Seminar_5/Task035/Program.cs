a77eeb97cb6d3b8a644a1d69c374c5be40009d0b
Alena <ABelova0605@gmail.com>
2022-08-10 20:25:36 +0300

    add folder Task035 in Seminar_5

---
+++ Seminar_5/Task035/Program.cs
@@ -0,0 +1,45 @@
+﻿// _____________Блок вспомогательных методов_____________
+//
+//_______________________________________________________
+
+void FillArray(int[] array, int startNumber = -10, int finishNumber = 10)
+{
+    finishNumber++;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(startNumber, finishNumber);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+// _____________Блок решения задачи_____________
+//
+// _____________________________________________
+
+/* 
+35. Задайте одномерный массив из 10 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+*/
+
+int size = 10;
+int[] array = new int[size];
+
+FillArray(array, -150, 151);
+PrintArray(array);
+
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] >= 10 && array[i] < 100) count++;
+}
+
+Console.WriteLine(count + " чисел в массиве находятся в промежутке [10, 99]");
