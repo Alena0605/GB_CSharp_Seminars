46f339c3e9eca85b89f7b0533fcf181c1e492e25
Alena <ABelova0605@gmail.com>
2022-08-11 18:17:57 +0300

    add folder Task042 in Seminar_6

---
+++ Seminar_6/Task042/Program.cs
@@ -0,0 +1,15 @@
+﻿// 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int bias = 1;
+
+while (number > 0)
+{
+    result = result + number % 2 * bias;
+    bias *= 10;
+    number /= 2;
+}
+
+Console.Write(result);
