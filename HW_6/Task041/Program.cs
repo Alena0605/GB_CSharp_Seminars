bb639868ceb1d3e23870120c79e2a09458fcac76
Alena <ABelova0605@gmail.com>
2022-08-11 19:53:41 +0300

    add folder Task041 in HW_6

---
+++ HW_6/Task041/Program.cs
@@ -0,0 +1,15 @@
+﻿// 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Сколько чисел вы хотите ввести: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for (int i = 1; i <= numbers; i++)
+{
+    Console.Write($"Введите {i}-е число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if (number > 0) count++;
+}
+
+Console.WriteLine($"Из введенных чисел больше 0: {count}");
