29fc21568c482cb356a62f1d480390f626862564
Alena <ABelova0605@gmail.com>
2022-08-19 22:01:56 +0300

    add folder Task048 in Seminar_7

---
+++ Seminar_7/Task048/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+48. Задайте двумерный массив размера M на N, каждый элемент в массиве находится по формуле: A = i + j.
+Выведите полученный массив на экран.
+*/
+
+Random random = new Random();
+
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+{
+    finishNumber++;
+    Random random = new Random();
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
