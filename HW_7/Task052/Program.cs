164854e1dfc51a969cfd46c8dc9aceb28279c59b
Alena <ABelova0605@gmail.com>
2022-08-19 23:41:44 +0300

    add folder Task052 in HW_7

---
+++ HW_7/Task052/Program.cs
@@ -0,0 +1,70 @@
+﻿// 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Random random = new Random();
+
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+FindColumnsAverage(array);
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+{
+    finishNumber++;
+    Random random = new Random();
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(startNumber, finishNumber);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindColumnsAverage (int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    int sum = 0;
+    
+    for (int j = 0; j < columns; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            sum += array[i, j];
+        }
+        double average = (double) sum / rows;
+        Console.WriteLine($"Среднее арифметическое в {j+1} столбце равно: {Math.Round(average, 2)}.");
+        
+        sum = 0;
+    }  
+}
