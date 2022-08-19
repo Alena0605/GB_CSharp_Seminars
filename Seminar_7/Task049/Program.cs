2579dce9c2676194041f2f9af589e805747d8af6
Alena <ABelova0605@gmail.com>
2022-08-19 22:26:53 +0300

    add folder Task049 in Seminar_7

---
+++ Seminar_7/Task049/Program.cs
@@ -0,0 +1,61 @@
+﻿/* 
+49. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.
+*/
+
+Random random = new Random();
+
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+for (int k = 0; k < rows; k += 2)
+{
+    for (int l = 0; l < columns; l += 2)
+    {
+        array[k, l] *= array[k, l];
+    }
+}
+
+Console.WriteLine();
+PrintArray(array);
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+{
+    finishNumber++;
+    Random random = new Random();
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(startNumber, finishNumber);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
