e710cc6a383ca32c3bd730ec59aee5a21ffeb738
Alena <ABelova0605@gmail.com>
2022-08-19 21:49:26 +0300

    add folder Task046 in Seminar_7

---
+++ Seminar_7/Task046/Program.cs
@@ -0,0 +1,47 @@
+﻿// 46. Задайте двумерный массив размером M × N, заполненный случайными целыми числами.
+
+Random random = new Random();
+
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+{
+    finishNumber++;
+    Random random = new Random();
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(startNumber, finishNumber);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
