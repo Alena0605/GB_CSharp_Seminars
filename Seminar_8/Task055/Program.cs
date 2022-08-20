c299d2d9dd40dcb47d77809f59d40ed4f5d55b37
Alena <ABelova0605@gmail.com>
2022-08-20 18:10:25 +0300

    add folder Task055 in Seminar_8

---
+++ Seminar_8/Task055/Program.cs
@@ -0,0 +1,66 @@
+﻿/* 
+55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+*/
+
+Random random = new Random();
+
+int rows = random.Next(4, 6);
+int columns = random.Next(4, 6);
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+if (rows == columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < columns; j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    }
+    Console.WriteLine();
+    PrintArray(array);
+}
+else Console.WriteLine("Количество столбцов и строк матрицы должно быть равно.");
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void FillArray(int[,] array, int startNumber = 0, int finishNumber = 10)
+{
+    finishNumber++;
+    Random random = new Random();
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(startNumber, finishNumber);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
