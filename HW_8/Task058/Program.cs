9c8fc86bbbbe9b88b1684de684831a7686e02173
Alena <ABelova0605@gmail.com>
2022-08-20 23:34:44 +0300

    add folder Task058 in HW_8

---
+++ HW_8/Task058/Program.cs
@@ -0,0 +1,42 @@
+﻿// 58.  Заполните спирально массив 4 на 4 числами от 1 до 16.
+
+int size = 4;
+int[,] array = new int[size, size];
+
+int numbers = size * size;
+int number = 1;
+int i = 0;
+int j = 0;
+
+while (number <= numbers)
+{
+    array[i, j] = number;
+    if (i <= j + 1 && i + j < size - 1) j++;
+    else if (i < j && i + j >= size - 1) i++;
+    else if (i >= j && i + j > size - 1) j--;
+    else i--;
+
+    number++;
+}
+
+PrintArray(array);
+
+
+// _______________БЛОК МЕТОДОВ_______________
+//
+//___________________________________________
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
