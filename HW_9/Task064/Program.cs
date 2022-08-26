cb68ce01df9f1d42c9121028c626e830d5bfd09a
Alena <ABelova0605@gmail.com>
2022-08-26 19:29:40 +0300

    add folder Task064 in HW_9

---
+++ HW_9/Task064/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+64. Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+*/
+
+void NumbersMultiplyOfThree(int m, int n)
+{
+    if (m > n) return;
+    
+    if (m % 3 != 0) m++;
+    else
+    {
+        Console.Write(m + "  ");
+        m += 3;
+    }
+    NumbersMultiplyOfThree(m, n);
+}
+
+NumbersMultiplyOfThree(15, 29);
