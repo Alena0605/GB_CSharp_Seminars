1fd80b3e60587933afcf79c81f42bb1b391ff9cb
Alena <ABelova0605@gmail.com>
2022-08-26 19:11:14 +0300

    add folder Task069 in Seminar_9

---
+++ Seminar_9/Task069/Program.cs
@@ -0,0 +1,14 @@
+﻿/* 
+69. Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+*/
+
+int FindPow(int a, int b, int result = 1)
+{
+    if (b == 0) return result;
+    result *= a;
+    b--;
+    return FindPow(a, b, result);
+}
+
+Console.WriteLine(FindPow(2, 10));
