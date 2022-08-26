4a0113973cee64b62fa87d0fe5e5004746466ed4
Alena <ABelova0605@gmail.com>
2022-08-26 18:23:41 +0300

    add folder Task063 in Seminar_9

---
+++ Seminar_9/Task063/Program.cs
@@ -0,0 +1,11 @@
+﻿// 63.  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+void Recursion(int number, int counter = 1)
+{
+    if (counter > number) return;
+    Console.WriteLine(counter);
+    counter++;
+    Recursion(number, counter);
+}
+
+Recursion(5);
