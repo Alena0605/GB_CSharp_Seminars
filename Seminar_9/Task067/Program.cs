abebe93b64477de74ddc19b6234f43b9dbef17ab
Alena <ABelova0605@gmail.com>
2022-08-26 18:57:37 +0300

    add folder Task067 in Seminar_9

---
+++ Seminar_9/Task067/Program.cs
@@ -0,0 +1,15 @@
+﻿// 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+void SumOfDigits(int number, int sum = 0)
+{
+    if (number == 0) 
+    {
+        Console.WriteLine("Сумма цифр в числе равна: " + sum);
+        return;
+    }
+    sum += number % 10;
+    number /= 10;
+    SumOfDigits(number, sum);
+}
+
+SumOfDigits(589);
