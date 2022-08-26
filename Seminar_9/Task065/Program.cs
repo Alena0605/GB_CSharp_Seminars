19c845a865b64b064380c561aa8c7251a95469d9
Alena <ABelova0605@gmail.com>
2022-08-26 18:34:02 +0300

    add folder Task065 in Seminar_9

---
+++ Seminar_9/Task065/Program.cs
@@ -0,0 +1,11 @@
+﻿// 65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void Recursion(int m, int n)
+{
+    if (m > n) return;
+    Console.Write(m + " ");
+    m++;
+    Recursion(m, n);
+}
+
+Recursion(4, 8);
